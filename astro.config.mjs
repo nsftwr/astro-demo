32c9aec30909af4ebc1b0a511eb83d476a636bc4
Laura Naumenko <lauranaumenko@Black-Beauty.local>
2026-01-19 20:53:18 +0000

    uncomment

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://nsftwr.github.io",
-  base: '/astro-demo',
+  site: "https://nsftwr.github.io/astro-demo",
   output: 'static',
   integrations: [mdx(), sitemap(), icon(), react()],
   vite: {
